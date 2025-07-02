8592b04ba068d488870815567df7f7e011e5bca6
swbam <2728451+swbam@users.noreply.github.com>
2025-07-02 10:40:55 -0500

    edits

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,3 +1,4 @@
 export * from './src/client';
 export * from './src/supabase';
 export * from './src/schema';
+export * from './src/realtime';
--- packages/external-apis/src/index.ts
+++ packages/external-apis/src/index.ts
@@ -3,13 +3,13 @@ export * from './clients/spotify';
 export * from './clients/ticketmaster';
 export * from './clients/setlistfm';
 
+// Sync Scheduler singleton must be created before exporting
+import { SyncScheduler } from './services';
+export const syncScheduler = new SyncScheduler();
+
 // Sync Services
 export * from './services';
 export { SyncScheduler } from './services';
 
 // Utilities
 export * from './utils';
-
-// Provide a default singleton instance for convenience and backwards compatibility
-import { SyncScheduler } from './services';
-export const syncScheduler = new SyncScheduler();
